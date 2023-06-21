f989e491f3019cdf85282601859e547c5043c4be
neeswebservices <neeswebservice@gmail.com>
2023-06-21 10:23:11 +0545

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('what is this');
+import app from '@/app';
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import request from 'supertest';
 import { expect } from 'chai';
-import app from '@src/app';
+import app from '@/app';
 
 describe('App', () => {
   before((done) => {
