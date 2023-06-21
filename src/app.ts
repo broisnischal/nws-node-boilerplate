b76d0d47d20e28afd75aa2174740a1253d3eb9d2
neeswebservices <neeswebservice@gmail.com>
2023-06-21 11:36:13 +0545

    init-head

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app: Application = express();
 app.use(express.json({ limit: '5mb' }));
 app.use(sanitizeInput);
 
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 app.post('/test', (_req: Request, _res: Response) => {
   //   const additionalParams = { res: 'asdf' };
   //   const response = new HttpResponse(200, 'OK', { test: req.body }, additionalParams);
